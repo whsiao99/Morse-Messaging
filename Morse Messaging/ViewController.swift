be29c8d926dcc1c4b4cdfb4315560704a10fab1e
William Hsiao <second.hsiao@gmail.com>
2017-12-04 19:01:37 -0500

    Added a few empty button codes

--- Morse Messaging/ViewController.swift
+++ Morse Messaging/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var typeField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func dotPressed(_ sender: UIButton) {
+        typeField.text?.append(".")
+    }
+    
+    @IBAction func dashPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func backspacePressed(_ sender: UIButton) {
+        // Delete last . or - entered when last button was not end char
+        // Else delete last char
+    }
+    
+    @IBAction func endCharPressed(_ sender: UIButton) {
+        // Take stored . and dash combination and translate to a character
+    }
+    
 }
-
