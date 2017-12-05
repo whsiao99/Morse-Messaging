e4e49f835a5f5096a5a88b558f95b4ca007fec4b
Christopher Kucera <34083510+1CRK@users.noreply.github.com>
2017-12-05 17:11:20 -0500

    Update ViewController.swift

--- Morse Messaging/ViewController.swift
+++ Morse Messaging/ViewController.swift
@@ -43,6 +43,122 @@ class ViewController: UIViewController {
     @IBAction func endCharPressed(_ sender: UIButton) {
         // Take stored . and dash combination and translate to a character
     }
+    //Convert Moorse code to english characters:
+    /*func morseToChar(morse: String) -> String {
+        var engChar=""
+        if morse==".--"{
+            engChar="A"
+        }
+        else if morse=="-..."{
+            engChar="B"
+        }
+        else if morse=="-.-."{
+            engChar="C"
+        }
+        else if morse=="-.."{
+            engChar="D"
+        }
+        else if morse=="."{
+            engChar="E"
+        }
+        else if morse=="..-."{
+            engChar="F"
+        }
+        else if morse=="--."{
+            engChar="G"
+        }
+        else if morse=="...."{
+            engChar="H"
+        }
+        else if morse==".."{
+            engChar="I"
+        }
+        else if morse==".---"{
+            engChar="J"
+        }
+        else if morse=="-.-"{
+            engChar="K"
+        }
+        else if morse==".-.."{
+            engChar="L"
+        }
+        else if morse=="--"{
+            engChar="M"
+        }
+        else if morse=="-."{
+            engChar="N"
+        }
+        else if morse=="---"{
+            engChar="O"
+        }
+        else if morse==".--."{
+            engChar="P"
+        }
+        else if morse=="--.-"{
+            engChar="Q"
+        }
+        else if morse==".-."{
+            engChar="R"
+        }
+        else if morse=="..."{
+            engChar="S"
+        }
+        else if morse=="-"{
+            engChar="G"
+        }
+        else if morse=="..-"{
+            engChar="U"
+        }
+        else if morse=="...-"{
+            engChar="V"
+        }
+        else if morse==".--"{
+            engChar="W"
+        }
+        else if morse=="-..-"{
+            engChar="X"
+        }
+        else if morse=="-.--"{
+            engChar="Y"
+        }
+        else if morse=="--.."{
+            engChar="Z"
+        }
+        else if morse==".----"{
+            engChar="1"
+        }
+        else if morse=="..---"{
+            engChar="2"
+        }
+        else if morse=="...--"{
+            engChar="3"
+        }
+        else if morse=="....-"{
+            engChar="4"
+        }
+        else if morse=="....."{
+            engChar="5"
+        }
+        else if morse=="-...."{
+            engChar="6"
+        }
+        else if morse=="--..."{
+            engChar="7"
+        }
+        else if morse=="---.."{
+            engChar="8"
+        }
+        else if morse=="----."{
+            engChar="9"
+        }
+        else if morse=="-----"{
+            engChar="0"
+        }
+        else {
+            //Give user alert that what they entered is not a number or character
+        }
+        return engChar;
+    }*/
     
 }
 
