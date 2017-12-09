5051d939f58b25e447fc8841854ffb907a0afa59
William Hsiao <second.hsiao@gmail.com>
2017-12-08 23:16:28 -0500

    Minor changes

--- Morse Messaging/ViewController.swift
+++ Morse Messaging/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        morseInput.sizeToFit()
+        morseInput.textAlignment = NSTextAlignment.center
         morseInput.text = ""
         // Do any additional setup after loading the view, typically from a nib.
     }
